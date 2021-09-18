3817ba027b7e53be00105b75e944b863d2ed0b1c
xhaoh94 <xhaoh94@qq.com>
2021-09-18 11:58:57 +0800

    up

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"pbtool/conf"
 	"pbtool/forms"
+	"pbtool/parse"
 
 	_ "github.com/ying32/govcl/pkgs/winappres"
 	"github.com/ying32/govcl/vcl"
@@ -10,17 +13,32 @@ import (
 )
 
 func main() {
+	cfgId := *flag.Int("id", 0, "生产代码类型")
+	flag.Parse()
 	if conf.ReadCfg() {
-		runApp()
+		runApp(cfgId)
 	}
 }
 
-func runApp() {
+func runApp(cfgId int) {
 
-	vcl.Application.Initialize()
-	vcl.Application.SetMainFormOnTaskBar(true)
-	vcl.Application.CreateForm(&forms.MainForm)
-	vcl.Application.CreateForm(&forms.CfgForm)
-	vcl.Application.Run()
+	if cfgId == 0 {
+		vcl.Application.Initialize()
+		vcl.Application.SetMainFormOnTaskBar(true)
+		vcl.Application.CreateForm(&forms.MainForm)
+		vcl.Application.CreateForm(&forms.CfgForm)
+		vcl.Application.Run()
+	} else {
+		cfg, ok := conf.GetCfgs()[cfgId]
+		if !ok {
+			fmt.Printf("没有找到对应的配置ID:[%d]", cfgId)
+			return
+		}
+		b := parse.Parse(cfg)
+		if b {
+			fmt.Println("导出成功")
+		}
+	}
+
 	// vcl.RunApp(&forms.MainForm, &forms.CfgForm)
 }
